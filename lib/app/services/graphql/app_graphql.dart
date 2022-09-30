88bb1eeac76c5689d0bac1f82fac1032e70f3760
Alexey Zdorovykh <47769319+Overman775@users.noreply.github.com>
2022-09-30 19:46:47 +0500

    ferry client

--- lib/app/services/graphql/app_graphql.dart
+++ lib/app/services/graphql/app_graphql.dart
@@ -1,26 +1,40 @@
 import 'dart:io';
 
-import 'package:artemis/artemis.dart';
 import "package:dio/dio.dart";
+import 'package:ferry/ferry.dart';
+import 'package:gql/ast.dart';
 import 'package:gql_dio_link/gql_dio_link.dart';
+import 'package:gql_exec/gql_exec.dart';
 import 'package:gql_websocket_link/gql_websocket_link.dart';
 
 class AppGraphQLArtemisClient {
-  late final ArtemisClient client;
-  late final ArtemisClient socket;
+  late final Client client;
+
+  late final DioLink _dioLink;
+  late final WebSocketLink _socketLink;
 
   AppGraphQLArtemisClient(Dio dio) {
-    final dioLink = DioLink("http://$_host:8080/graphql", client: dio);
+    _dioLink = DioLink("http://$_host:8080/graphql", client: dio);
 
-    client = ArtemisClient.fromLink(dioLink);
-
-    final socketLink = WebSocketLink(
+    _socketLink = WebSocketLink(
       'ws://$_host:8080/subscriptions',
       autoReconnect: true,
     );
 
-    socket = ArtemisClient.fromLink(socketLink);
+    final linkRouter = Link.route(_getLink);
+
+    client = Client(link: linkRouter);
   }
 
   String get _host => Platform.isAndroid ? '10.0.2.2' : 'localhost';
+
+  Link _getLink(Request request) {
+    final isSubscription = request.operation.document.definitions.any(
+      (definition) =>
+          definition is OperationDefinitionNode &&
+          definition.type == OperationType.subscription,
+    );
+
+    return isSubscription ? _socketLink : _dioLink;
+  }
 }
