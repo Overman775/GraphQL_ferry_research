a643d48f1f22b457c0c5dee84ad1e471d3406647
Alexey Zdorovykh <47769319+Overman775@users.noreply.github.com>
2022-10-05 00:36:48 +0500

    add proxy overides

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,8 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
 
 import 'app/app.dart';
-import 'app/services/proxy/proxy_http_overrides.dart';
 
 void main() {
-  HttpOverrides.global = ProxyHttpOverrides('<your api>:8888');
+  // HttpOverrides.global = ProxyHttpOverrides('<your api>:8888');
   runApp(const MyApp());
 }
